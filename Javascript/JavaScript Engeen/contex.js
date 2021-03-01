51032a0b753a483e9f9105afd3312428e61327e3
irfathima <irfatharaima@gmail.com>
2021-03-01 17:47:20 +0600

    Signed-off-by: irfathima <irfatharaima@gmail.com>

---
+++ Javascript/JavaScript Engeen/contex.js
@@ -0,0 +1,35 @@
+// execution of global contex
+
+/*function a(){
+	b()
+	console.log("I am Function A")
+}
+function b(){
+	d()
+	console.log("I am Function B")
+}
+function c(){
+	console.log("I am Function C")
+}
+function d(){
+	c()
+	console.log("I am Function D")
+}
+
+var x=100
+a()
+console.log("I am global")*/
+// ============================
+
+/*var a = 10
+var b
+
+console.log(a)
+console.log(b)
+b = 20
+
+console.log(c)
+var c = 50*/
+
+// =============
+function
